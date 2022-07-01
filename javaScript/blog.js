eaec92adc9bf836c24c2ca85ff8d2d6bb0905049
Kaustubh Dubey <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2022-07-02 01:34:51 +0530

    Update blog.js

---
+++ javaScript/blog.js
@@ -0,0 +1,45 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("moreText");
+    var btnText = document.getElementById("read-btn");
+ 
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunction1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("moreText1");
+    var btnText = document.getElementById("read-btn1");
+ 
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunction2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("moreText2");
+    var btnText = document.getElementById("read-btn2");
+ 
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
